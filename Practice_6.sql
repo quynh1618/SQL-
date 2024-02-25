27c045ce0cf0b07b5d9107795c77321a72196f07
quynh1618 <160311102+quynh1618@users.noreply.github.com>
2024-02-25 17:28:19 +0700

    Update Practice_6.sql

--- Practice_6.sql
+++ Practice_6.sql
@@ -49,3 +49,11 @@ WHERE a.Month = b.Month-1
 )
 SELECT Month, COUNT(user_id) AS monthly_active_users FROM twt_month
 GROUP BY Month
+-- EX6
+SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month, country,
+COUNT(id) AS trans_count,
+SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+SUM(amount) AS trans_total_amount,
+SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY DATE_FORMAT(trans_date, '%Y-%m'), country
