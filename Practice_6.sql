027f2b4362944ce24e1a3f599dd8ac6006df3a93
quynh1618 <160311102+quynh1618@users.noreply.github.com>
2024-02-26 22:25:59 +0700

    Update Practice_6.sql

--- Practice_6.sql
+++ Practice_6.sql
@@ -57,3 +57,53 @@ SUM(amount) AS trans_total_amount,
 SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
 FROM Transactions
 GROUP BY DATE_FORMAT(trans_date, '%Y-%m'), country
+-- EX7
+WITH twt_year_ranking AS(
+SELECT product_id, year, quantity, price,
+RANK()OVER (PARTITION BY product_id ORDER BY year) AS ranking
+FROM Sales)
+SELECT product_id, year AS first_year, quantity, price
+FROM twt_year_ranking
+WHERE ranking = 1
+-- EX8
+WITH twt_product_count AS(
+SELECT customer_id, product_key, COUNT(DISTINCT product_key) AS count
+FROM Customer
+GROUP BY customer_id)
+SELECT a.customer_id 
+FROM twt_product_count AS a
+JOIN Product AS b ON a.product_key=b.product_key
+WHERE a.count= (SELECT COUNT(DISTINCT product_key) FROM Product)
+-- EX 9
+WITH twt_salary_filter AS(
+SELECT employee_id, name, manager_id, salary FROM Employees
+WHERE salary < 30000 AND manager_id IS NOT NULL)
+SELECT a.employee_id
+FROM twt_salary_filter AS a
+LEFT JOIN twt_salary_filter AS b ON a.manager_id=b.employee_id
+WHERE b.employee_id IS NULL
+ORDER BY a.employee_id
+-- EX10
+WITH twt_job_count AS
+(SELECT company_id, title, description, COUNT(job_id) AS job_count 
+FROM job_listings
+GROUP BY company_id, title, description)
+SELECT COUNT (DISTINCT company_id) AS duplicate_companies
+FROM twt_job_count
+WHERE job_count > 1
+-- EX 11 (nhờ mn chữa giúp em bài này ạ)
+-- EX 12 (nhờ mn chữa giúp em bài này, em chạy không giống kq ạ)
+WITH CTE AS(
+SELECT ids, count FROM
+(SELECT requester_id AS ids, COUNT(accepter_id) AS count
+FROM RequestAccepted 
+GROUP BY requester_id
+UNION
+SELECT accepter_id AS ids, COUNT(requester_id) AS count
+FROM RequestAccepted
+GROUP BY accepter_id) AS SUB
+)
+SELECT ids AS id, count AS num
+FROM CTE
+GROUP BY id
+ORDER BY num DESC
