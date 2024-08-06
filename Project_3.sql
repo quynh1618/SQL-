4edd7d4958ed0cf52f912f77598be37eed647e00
quynh1618 <160311102+quynh1618@users.noreply.github.com>
2024-08-06 18:20:52 +0700

    Quynh Nguyen_Project_3.sql

--- Project_3.sql
+++ Project_3.sql
@@ -1,3 +1,16 @@
+-- ĐỀ BÀI:
+Sau khi dữ liệu được làm sạch hãy tiến hành phân tích theo các gợi ý sau:
+1) Doanh thu theo từng ProductLine, Year  và DealSize?
+Output: PRODUCTLINE, YEAR_ID, DEALSIZE, REVENUE
+2) Đâu là tháng có bán tốt nhất mỗi năm?
+Output: MONTH_ID, REVENUE, ORDER_NUMBER
+3) Product line nào được bán nhiều ở tháng 11?
+Output: MONTH_ID, REVENUE, ORDER_NUMBER
+4) Đâu là sản phẩm có doanh thu tốt nhất ở UK mỗi năm? 
+Xếp hạng các các doanh thu đó theo từng năm.
+Output: YEAR_ID, PRODUCTLINE,REVENUE, RANK
+5) Ai là khách hàng tốt nhất, phân tích dựa vào RFM 
+-- PHẦN THỰC HÀNH
 -- Câu 1
 SELECT PRODUCTLINE, YEAR_ID, DEALSIZE, SUM(sales) AS REVENUE
 FROM SALES_DATASET_RFM_PRJ
